938f44195ab5e57199ac07e02feab178569143cf
Anastasiia Mishchenko <mishchenko.anastasiia@gmail.com>
2021-02-07 15:44:52 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/858cea01db";
+
+  const options = {
+    method: "POST",
+    auth: "AnastasiiaMP:c496567632267921243bf7e193c6adeb-us7"
+
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+    console.log(JSON.parse(data));
+
+    })
+
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server on a port 3000 is listening");
+});
+
+//Api key
+//c496567632267921243bf7e193c6adeb-us7
+
+//Id list
+//858cea01db
